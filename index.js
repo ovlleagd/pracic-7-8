aa4cca91f2856ae5252c80c07966d53ffd2d7133
Marina <megorodkova999@gmail.com>
2025-01-26 11:53:34 +0100

    task7-8

--- index.js
+++ index.js
@@ -57,13 +57,36 @@
 // }
 
 //task 6
-const title = document.querySelector("h1");
-const list = document.createElement("ul");
-let message = prompt();
-while (message !== null) {
-  const listItem = document.createElement("li");
-  listItem.textContent = message;
-  list.appendChild(listItem);
-  message = prompt();
+// const title = document.querySelector("h1");
+// const list = document.createElement("ul");
+// let message = prompt();
+// while (message !== null) {
+//   const listItem = document.createElement("li");
+//   listItem.textContent = message;
+//   list.appendChild(listItem);
+//   message = prompt();
+// }
+// title.after(list);
+
+// task 7
+// const links = document.querySelectorAll("a");
+// links.forEach((link) => {
+//   link.addEventListener("mouseover", addTitle);
+// });
+// function addTitle(e) {
+//   e.target.title = e.target.textContent;
+// }
+
+// task 8
+const spans = document.querySelectorAll("span");
+spans.forEach((span) => span.addEventListener("click", setBgRed));
+function setBgRed(e) {
+  e.target.style.backgroundColor = "red";
+  e.target.addEventListener("click", setBgGreen);
+  e.target.removeEventListener("click", setBgRed);
 }
-title.after(list);
+function setBgGreen(e) {
+  e.target.style.backgroundColor = "green";
+  e.target.addEventListener("click", setBgRed);
+  e.target.removeEventListener("click", setBgGreen);
+}
